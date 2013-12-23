228cf9bb0f27ae2e7423f03085516fef760c0e9c
Dan Stoner <danstoner@gmail.com>
2013-12-23 08:43:30 -0500

    ex5 convert some pounds to kilos

--- ex5.py
+++ ex5.py
@@ -16,3 +16,5 @@ print "His teeth are usually %s depending on the coffee." % teeth
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
     age, height, weight, age + height + weight)
+
+print "His weight in kilos is", weight * 0.453592
