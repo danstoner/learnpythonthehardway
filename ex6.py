6ce4af22b33d95b5678567a2073e6875dda937df
Dan Stoner <danstoner@gmail.com>
2013-12-24 18:22:31 -0500

    minor typo

--- ex6.py
+++ ex6.py
@@ -10,7 +10,7 @@ print "I said: %r." % x
 print "I also said: '%s'." % y
 
 hilarious = False
-joke_evaluation = "Isn't this joke so funny?! %r"
+joke_evaluation = "Isn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
 
