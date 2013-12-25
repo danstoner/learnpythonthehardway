0c8be205216754ee6c627eb4507834432e1c15d4
Dan Stoner <danstoner@gmail.com>
2013-12-24 22:29:39 -0500

    exercise 10

---
+++ ex10.py
@@ -0,0 +1,24 @@
+# exercise 10
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#  sample tiny piece of fun code
+count = 0
+while (count < 1000):
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i,
+    count = count + 1
