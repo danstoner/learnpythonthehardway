fe440b580c1ad0ea1a32babe63874f779340bce2
Dan Stoner <danstoner@gmail.com>
2013-12-25 22:36:38 -0500

    finished exercise 13

---
+++ ex13.py
@@ -0,0 +1,12 @@
+# exercise 13
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+color = raw_input("What is your favorite color? ")
+print "Your favorite color is: %s" % (color)
