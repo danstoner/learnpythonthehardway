7e8fb8c326246c7136198080c48f3c100e3d3d27
Dan Stoner <danstoner@gmail.com>
2014-02-20 10:01:54 -0500

    function can now accept an increment value

--- ex33.py
+++ ex33.py
@@ -1,20 +1,27 @@
 numbers = []
 
-def do_loop(thelist,max):
+def do_loop(thelist,max,inc):
+    """This function accepts a list, the max value, and the increment by value."""
     i = 0
     while i < max:
         print "At the top i is %d" % i
         numbers.append(i)
-        i = i+1
+        i = i+inc
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
     return thelist
 
-numbers = do_loop(numbers,6)
+numbers = do_loop(numbers,6,1)
 
 
 print "The numbers: "
 
+for num in numbers:
+    print num
+
+numbers = []
+numbers = do_loop(numbers,6,2)
+print "AGAIN the numbers by twos..."
 for num in numbers:
     print num
