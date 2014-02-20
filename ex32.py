30c7c8646c4d9c34aa3a882839e6f241e3fe5b13
Dan Stoner <danstoner@gmail.com>
2014-02-20 09:48:24 -0500

    completed exercise 32

---
+++ ex32.py
@@ -0,0 +1,40 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print "This is count %d" % number
+
+# same as above
+for fruit in fruits:
+    print "A fruit of type: %s" % fruit
+
+# also we can go through mixed lists too
+# notice we have to use %r since we con't know what's in it
+for i in change:
+    print "I got %r" % i
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# pydoc range
+# range(...)
+#     range([start,] stop[, step]) -> list of integers
+
+#     Return a list containing an arithmetic progression of integers.
+#     range(i, j) returns [i, i+1, i+2, ..., j-1]; start (!) defaults to 0.
+#     When step is given, it specifies the increment (or decrement).
+#     For example, range(4) returns [0, 1, 2, 3].  The end point is omitted!
+#     These are exactly the valid indices for a list of 4 elements.
+
+
+# then use the range function to do 0 to 5 counts
+for i in range (6):
+    print "Adding %d to the list." % i
+    # append is a function that lists understand
+    elements.append(i)
+
+# now we can print them out too
+for i in elements:
+    print "Element was: %d" % i
